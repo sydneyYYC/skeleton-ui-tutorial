e05cd857bed9110470050f41154a9869dccbcd05
Sydney Bruce <sydneybruce20@gmail.com>
2023-11-08 11:11:42 -0700

    paying with plug ins - broke site

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,44 +1,51 @@
 <script>
 	import '../app.postcss';
-	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import { AppShell, AppBar, Avatar } from '@skeletonlabs/skeleton';
 </script>
 
 <!-- App Shell -->
-<AppShell>
-	<svelte:fragment slot="header">
-		<!-- App Bar -->
-		<AppBar>
-			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Skeleton</strong>
-			</svelte:fragment>
-			<svelte:fragment slot="trail">
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
-				</a>
-			</svelte:fragment>
-		</AppBar>
-	</svelte:fragment>
-	<!-- Page Route Content -->
+<AppBar>
+	<svelte:fragment slot="header">Header</svelte:fragment>
+	<svelte:fragment slot="sidebarLeft">Sidebar Left</svelte:fragment>
+	<!-- (sidebarRight) -->
+	<svelte:fragment slot="pageHeader">Page Header</svelte:fragment>
+	<!-- Router Slot -->
 	<slot />
+	<!-- ---- / ---- -->
+	<!-- (pageFooter) -->
+	<svelte:fragment slot="footer">&copy;Sydney</svelte:fragment>
+</AppBar>
+<AppShell>
+	<svelte:fragment slot="lead">
+		<strong class="text-xl uppercase">Skeleton</strong>
+		<p class="mx-4">Hello</p>
+	</svelte:fragment>
+	<svelte:fragment slot="trail">
+		<a
+		class="btn btn-sm variant-ghost-surface"
+			href="https://discord.gg/EXqV7W8MtY"
+			target="_blank"
+			rel="noreferrer"
+			>
+			Discord
+		</a>
+		<a
+		class="btn btn-sm variant-ghost-surface"
+		href="https://twitter.com/SkeletonUI"
+		target="_blank"
+		rel="noreferrer"
+		>
+		Twitter
+	</a>
+	<a
+	class="btn btn-sm variant-ghost-surface"
+	href="https://github.com/skeletonlabs/skeleton"
+	target="_blank"
+	rel="noreferrer"
+	>
+	GitHub
+</a>
+</svelte:fragment>
 </AppShell>
+<!-- App Bar -->
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
-
+<script>
+	import { Accordion, AccordionItem } from '@skeletonlabs/skeleton';
+</script>
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-10 text-center flex flex-col items-center">
 		<h2 class="h2">Welcome to Skeleton.</h2>
@@ -32,8 +34,10 @@
 			<p>Try editing the following:</p>
 			<p><code class="code">/src/routes/+layout.svelte</code></p>
 			<p><code class="code">/src/routes/+page.svelte</code></p>
+			<Accordion {AccordionItem}/> 
 		</div>
 	</div>
+	
 </div>
 
 <style lang="postcss">
